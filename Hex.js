3c203fae842312010ffc951a3eb06d64bd06edb2
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-02-02 11:28:30 +0530

    Update Hex.js

--- Hex.js
+++ Hex.js
@@ -4,9 +4,11 @@ class Hex {
         'density':1.5,
         'friction': 1.0,
         'restitution':0.5
+
       };
       this.body = Bodies.rectangle(x, y, 50, 50, options);
       this.width = 50;
+      this.image = loadImage("polygon.png");
       this.height = 50;
       World.add(world, this.body);
       //this.Visibility= 255;
@@ -16,15 +18,20 @@ class Hex {
      // pos.x = mouseX;
      // pos.y = mouseY;
       var angle = this.body.angle;
-  
+
+      imageMode(CENTER);
+      image(this.image, pos.x,pos.y, 100, 100);
       push();
       translate(pos.x, pos.y);
       rotate(angle);
       strokeWeight(3);
-      stroke('blue')
-      fill('red')
-      rectMode(CENTER)
-      rect(0, 0, this.width, this.height);
+      stroke('blue');
+      fill('red');
+     // rectMode(CENTER);
+     // rect(0, 0, this.width, this.height);
+      
+     
+  
 
       pop();
 
